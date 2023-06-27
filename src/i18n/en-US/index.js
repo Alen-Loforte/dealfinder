f68d91b56a4a2ca28f52778db1045702c13d7e7f
Alen Loforte <53468037+Alen-Loforte@users.noreply.github.com>
2023-06-27 09:37:36 +0200

    Add new translations

--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -2,6 +2,14 @@
 // so you can safely delete all default props below
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
-}
+  failed: "Action failed",
+  success: "Action was successful",
+  title: "Title: ",
+  price: "Price: ",
+  salePrice: "Sale price: ",
+  store: "Store: ",
+  dealRating: "Deal Rating: ",
+  metaCriticScore: "MetaCritic Score: ",
+  steamScore: "Steam Rating: ",
+  releaseDay: "Release day: ",
+};
