d694bf2343d7417172fb5e86c17a9b52f308f957
Alen Loforte <53468037+Alen-Loforte@users.noreply.github.com>
2023-06-27 09:28:36 +0200

    Add new global import

--- src/boot/global-imports.js
+++ src/boot/global-imports.js
@@ -1,9 +1,11 @@
 import BaseInput from "../components/Base/BaseInput.vue";
 import BasePage from "../components/Base/BasePage.vue";
 import BaseIcon from "../components/Base/BaseIcon.vue";
+import BaseGameCard from "../components/Base/BaseGameCard.vue";
 
 export default ({ app }) => {
   app.component("BaseInput", BaseInput);
   app.component("BasePage", BasePage);
   app.component("BaseIcon", BaseIcon);
+  app.component("BaseGameCard", BaseGameCard);
 };
