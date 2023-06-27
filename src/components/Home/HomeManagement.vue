ca05d3bd2a74d07eb84a38fe88e65d7191774143
Alen Loforte <53468037+Alen-Loforte@users.noreply.github.com>
2023-06-27 09:35:45 +0200

    Create HomeManagement.vue

---
+++ src/components/Home/HomeManagement.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="content-container">
+      <BaseGameCard
+        v-for="dealItem in dealList"
+        :key="dealItem.gameID"
+        :dealDetail="dealItem"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import { useDealStore } from "src/stores/deal-store";
+const store = useDealStore();
+const { dealList } = storeToRefs(store);
+
+onMounted(() => {
+  store.getDealsList();
+});
+</script>
+
+<style lang="scss" scoped>
+.content-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1.85rem;
+}
+</style>
