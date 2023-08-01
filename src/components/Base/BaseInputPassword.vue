5132e7cdc08806e5cd75bbd64b132e50b259719f
Alen Loforte <53468037+Alen-Loforte@users.noreply.github.com>
2023-08-01 09:21:32 +0200

    Create BaseInputPassword.vue

---
+++ src/components/Base/BaseInputPassword.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-input
+    :model-value="value"
+    :label="required == true ? label + ' *' : label"
+    :placeholder="placeholder"
+    :type="isPassword ? 'password' : 'text'"
+    :mask="mask"
+    :readonly="isReadonly"
+    :disable="isDisabled"
+    :rules="rules"
+    :hint="hint"
+    class="global-form-control"
+    filled
+    @update:model-value="(value) => $emit('update:text', value)"
+  >
+    <template v-slot:append>
+      <q-icon
+        :name="isPassword ? 'visibility_off' : 'visibility'"
+        class="cursor-pointer"
+        @click="isPassword = !isPassword"
+      />
+    </template>
+  </q-input>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  value: {
+    type: [String, Number],
+    default: "",
+  },
+  label: {
+    type: String,
+    default: null,
+  },
+  placeholder: {
+    type: String,
+    default: null,
+  },
+  mask: {
+    type: String,
+    default: "",
+  },
+  hint: {
+    type: String,
+    default: null,
+  },
+  isReadonly: {
+    type: Boolean,
+    default: false,
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  type: {
+    type: String,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  rules: {
+    type: Array,
+  },
+});
+
+const isPassword = ref(false);
+</script>
+
+<style lang="scss" scoped></style>
