c980adf777f9e7b296f066d97a41658239420534
Alen Loforte <53468037+Alen-Loforte@users.noreply.github.com>
2023-08-01 09:21:25 +0200

    Add imports to Base components

--- src/boot/global-imports.js
+++ src/boot/global-imports.js
@@ -2,10 +2,22 @@ import BaseInput from "../components/Base/BaseInput.vue";
 import BasePage from "../components/Base/BasePage.vue";
 import BaseIcon from "../components/Base/BaseIcon.vue";
 import BaseGameCard from "../components/Base/BaseGameCard.vue";
+import BaseInputPassword from "../components/Base/BaseInputPassword.vue";
+import BaseSelect from "../components/Base/BaseSelect.vue";
+import BaseButton from "../components/Base/BaseButton.vue";
+import BasePageLink from "../components/Base/BasePageLink.vue";
+import BaseTooltip from "../components/Base/BaseTooltip.vue";
+import BaseDropdownButton from "../components/Base/BaseDropdownButton.vue";
 
 export default ({ app }) => {
   app.component("BaseInput", BaseInput);
   app.component("BasePage", BasePage);
   app.component("BaseIcon", BaseIcon);
   app.component("BaseGameCard", BaseGameCard);
+  app.component("BaseSelect", BaseSelect);
+  app.component("BaseButton", BaseButton);
+  app.component("BaseInputPassword", BaseInputPassword);
+  app.component("BasePageLink", BasePageLink);
+  app.component("BaseTooltip", BaseTooltip);
+  app.component("BaseDropdownButton", BaseDropdownButton);
 };
