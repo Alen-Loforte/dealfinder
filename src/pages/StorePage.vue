6a8a2c14c0df899fe638f0b3e55f8c5dedd8b683
Alen Loforte <53468037+Alen-Loforte@users.noreply.github.com>
2023-10-21 21:17:06 +0200

    Create StorePage.vue

---
+++ src/pages/StorePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <BasePage>
+    <StoreManagement />
+  </BasePage>
+</template>
+
+<script setup>
+import StoreManagement from "src/components/Store/StoreManagement.vue";
+</script>
+
+<style lang="scss" scoped></style>
